c474f2ab6854e9ad5d4945658f85ccab841ef8fe
Davidchhing <davidhchhing@gmail.com>
2018-11-07 17:23:46 -0800

    added hamburger menu and jquery

---
+++ js/view.js
@@ -0,0 +1,19 @@
+'use strict'
+
+$(document).ready(function() {
+    $( ".cross" ).hide();
+    $( ".main-nav" ).hide();
+    $( ".hamburger" ).click(function() {
+        $( ".main-nav" ).slideToggle( "slow", function() {
+            $( ".hamburger" ).hide();
+            $( ".cross" ).show();
+        });
+    });
+    $( ".cross" ).click(function() {
+        $( ".main-nav" ).slideToggle( "slow", function() {
+            $( ".cross" ).hide();
+            $( ".hamburger" ).show();
+        });
+    });
+});
+
